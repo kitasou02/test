69d6c734e74a20e90f8d4ccdcc409d625066cf5e
kitasou02 <67031834+kitasou02@users.noreply.github.com>
2020-06-17 16:07:09 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,20 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (1 < 2) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-    if (1 == 1) {
+    let 相手の手２ = 0
+    let 相手の手１ = 0
+    let 自分の手 = false
+    if (自分の手 == (相手の手１ == 相手の手２)) {
         basic.showIcon(IconNames.Triangle)
-    }
-    if (2 < 3) {
-        basic.showIcon(IconNames.No)
-    } else {
-        basic.showIcon(IconNames.Heart)
-    }
-    if (2 == 2) {
+    } else if (false && (false && false)) {
         basic.showIcon(IconNames.Triangle)
-    }
-    if (3 < 1) {
-        basic.showIcon(IconNames.No)
     } else {
-        basic.showIcon(IconNames.Heart)
-    }
-    if (3 == 3) {
-        basic.showIcon(IconNames.Triangle)
+    	
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Scissors)
-    radio.sendNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Square)
-    radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -40,7 +24,6 @@ input.onButtonPressed(Button.B, function () {
         . # # # .
         . . . . .
         `)
-    radio.sendNumber(2)
 })
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
